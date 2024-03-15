e19ff26bb0bd13e437e87055db61244bab97036b
Moazam05 <salmanmoazam08@gmail.com>
2024-03-15 19:30:44 +0500

    SEC FILINGS section

--- newsroom/ftc.php
+++ newsroom/ftc.php
@@ -101,6 +101,142 @@ include('../header.php'); ?>
         </div>
 </section>
 
+<!-- 5th Section -->
+<div class="investBggray">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 ">
+                <div class="f_div">
+                    <h2 class="f_div_title">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 44 44" fill="none">
+                                <path d="M15.0443 21.9258H27.5859" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M15.0443 29.0928H22.8918" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M18.6276 10.8171H25.7943C29.3776 10.8171 29.3776 9.02539 29.3776 7.23372C29.3776 3.65039 27.5859 3.65039 25.7943 3.65039H18.6276C16.8359 3.65039 15.0443 3.65039 15.0443 7.23372C15.0443 10.8171 16.8359 10.8171 18.6276 10.8171Z" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M29.3776 7.26953C35.3439 7.59203 38.3359 9.79578 38.3359 17.9837V28.7337C38.3359 35.9004 36.5443 39.4837 27.5859 39.4837H16.8359C7.8776 39.4837 6.08594 35.9004 6.08594 28.7337V17.9837C6.08594 9.8137 9.07802 7.59203 15.0443 7.26953" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </span>
+                        SEC Filings
+                    </h2>
+                    <a href="https://www.sec.gov/edgar/browse/?CIK=1407878" target="_blank">
+                        See our fillings at SEC.gov
+                    </a>
+                    <h2 class="f_div_title">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="44" height="43" viewBox="0 0 44 43" fill="none">
+                                <path d="M15.0443 39.4176H29.3776C36.5801 39.4176 37.8701 36.5331 38.2464 33.0214L39.5901 18.6881C40.0739 14.3164 38.8197 10.751 31.1693 10.751H13.2526C5.60219 10.751 4.34803 14.3164 4.83178 18.6881L6.17553 33.0214C6.55178 36.5331 7.84178 39.4176 15.0443 39.4176Z" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M15.0443 10.7507V9.31732C15.0443 6.14607 15.0443 3.58398 20.7776 3.58398H23.6443C29.3776 3.58398 29.3776 6.14607 29.3776 9.31732V10.7507" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M25.7943 23.2926V25.0843C25.7943 25.1022 25.7943 25.1022 25.7943 25.1201C25.7943 27.0731 25.7764 28.6676 22.2109 28.6676C18.6634 28.6676 18.6276 27.091 18.6276 25.1381V23.2926C18.6276 21.501 18.6276 21.501 20.4193 21.501H24.0026C25.7943 21.501 25.7943 21.501 25.7943 23.2926Z" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M39.5005 19.709C35.3618 22.719 30.6318 24.5107 25.7943 25.1198" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M5.40511 20.1934C9.43636 22.9525 13.9872 24.6188 18.6276 25.1384" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </span>
+                        Transfer Agent
+                    </h2>
+                    <h3>
+                        Worldwide Stock Transfer, LLC
+                    </h3>
+                    <p>
+                        One University Plaza Suite 505
+                    </p>
+                    <p>
+                        Hackensack, Nj 07601
+                    </p>
+                    <p>Tel: (201) 820-2008 </p>
+                    <p>URL: www.worldwidestocktransfer.com</p>
+                </div>
+            </div>
+            <div class="col-lg-6 ">
+                <div class="s_div">
+
+                    <h2 class="f_div_title">
+                        <span><svg xmlns="http://www.w3.org/2000/svg" width="44" height="45" viewBox="0 0 44 45" fill="none">
+                                <path d="M30.4725 34.5297H23.3058L15.3329 39.9563C14.1504 40.763 12.5558 39.9014 12.5558 38.4347V34.5297C7.18081 34.5297 3.59747 30.8631 3.59747 25.3631V14.363C3.59747 8.86296 7.18081 5.19629 12.5558 5.19629H30.4725C35.8475 5.19629 39.4308 8.86296 39.4308 14.363V25.3631C39.4308 30.8631 35.8475 34.5297 30.4725 34.5297Z" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M21.5142 21.5674V21.1825C21.5142 19.9358 22.2667 19.2758 23.0192 18.7441C23.7538 18.2308 24.4883 17.5708 24.4883 16.3608C24.4883 14.6741 23.1625 13.3174 21.5142 13.3174C19.8658 13.3174 18.54 14.6741 18.54 16.3608" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M21.5061 25.9499H21.5222" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg></span>
+                        Investor FAQ
+                    </h2>
+                    <div class="faqs">
+                        <div class="accordion accordion-flush" id="investor-faqs">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="heading1">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-1" aria-expanded="false" aria-controls="faq-1">
+                                        What is the legal status of the company and fiscal year end?
+                                    </button>
+                                </h2>
+                                <div id="faq-1" class="accordion-collapse collapse" aria-labelledby="heading1" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">News is generally released by the company through newswire services such as BusinessWire and MarketWire. In the interest of fair disclosure to the general public, we refrain from answering questions from individual investors regarding the company’s development progress.</div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="heading2">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-2" aria-expanded="false" aria-controls="faq-2">
+                                        Who is your transfer agent? Who should I contact regarding my stock certificate(s)?
+                                    </button>
+                                </h2>
+                                <div id="faq-2" class="accordion-collapse collapse" aria-labelledby="heading2" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">News is generally released by the company through newswire services such as BusinessWire and MarketWire. In the interest of fair disclosure to the general public, we refrain from answering questions from individual investors regarding the company’s development progress.</div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="heading3">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-3" aria-expanded="false" aria-controls="faq-3">
+                                        I purchased stock from the company in a private placement, when do I receive my stock certificate?
+                                    </button>
+                                </h2>
+                                <div id="faq-3" class="accordion-collapse collapse" aria-labelledby="heading3" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">News is generally released by the company through newswire services such as BusinessWire and MarketWire. In the interest of fair disclosure to the general public, we refrain from answering questions from individual investors regarding the company’s development progress.</div>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="heading4">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-4" aria-expanded="false" aria-controls="faq-4">
+                                        How do I sell the shares that I purchased from the company?
+                                    </button>
+                                </h2>
+                                <div id="faq-4" class="accordion-collapse collapse" aria-labelledby="heading4" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">News is generally released by the company through newswire services such as BusinessWire and MarketWire. In the interest of fair disclosure to the general public, we refrain from answering questions from individual investors regarding the company’s development progress.</div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="heading5">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-5" aria-expanded="false" aria-controls="faq-5">
+                                        What is a Rule 144 legal opinion letter and how do I get one?
+                                    </button>
+                                </h2>
+                                <div id="faq-5" class="accordion-collapse collapse" aria-labelledby="heading5" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">News is generally released by the company through newswire services such as BusinessWire and MarketWire. In the interest of fair disclosure to the general public, we refrain from answering questions from individual investors regarding the company’s development progress.</div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="heading6">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-6" aria-expanded="false" aria-controls="faq-6">
+                                        When and how are news releases issued by the company?
+                                    </button>
+                                </h2>
+                                <div id="faq-6" class="accordion-collapse collapse" aria-labelledby="heading6" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">News is generally released by the company through newswire services such as BusinessWire and MarketWire. In the interest of fair disclosure to the general public, we refrain from answering questions from individual investors regarding the company’s development progress.</div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="heading7">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-7" aria-expanded="false" aria-controls="faq-7">
+                                        How can I receive on-going information about Digital Locations?
+                                    </button>
+                                </h2>
+                                <div id="faq-7" class="accordion-collapse collapse" aria-labelledby="heading7" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">News is generally released by the company through newswire services such as BusinessWire and MarketWire. In the interest of fair disclosure to the general public, we refrain from answering questions from individual investors regarding the company’s development progress.</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
 include('../footer.php'); ?>
