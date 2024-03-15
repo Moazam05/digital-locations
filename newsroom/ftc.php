9ccb3acf9c5fe9d25bb565d735fb3c61dcd3d4e3
Moazam05 <salmanmoazam08@gmail.com>
2024-03-15 16:15:43 +0500

    upcoming vote

--- newsroom/ftc.php
+++ newsroom/ftc.php
@@ -7,18 +7,42 @@ include('../header.php'); ?>
 <section class="hero-section">
     <div class="container">
         <div class="heading">
-
             <h1>The Federal Communications Commission
                 <br>
                 <span>has its eyes set</span> on space and satellite
                 <br>
                 communications.
             </h1>
-
         </div>
         <div class="satellite-img">
             <img src="../assets/img/FTC-Satellite.png" alt="satellite">
         </div>
+    </div>
+</section>
+
+<section class="upcoming-vote">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>
+                    With an upcoming vote, the FCC will begin to lay the groundwork for <span>needed rules and regulations</span> for the growing satellite Internet business.
+                </h2>
+                <p>The FCC wants to establish clear and transparent processes to support supplemental Internet coverage from space. </p>
+
+                <p>Connecting consumers to essential wireless services where traditional terrestrial mobile service is not available can be lifesaving in remote locations and can open up innovative opportunities for consumers and businesses, according to the FCC.  </p>
+
+                <p>
+                    Broadband Internet is a basic necessity in today’s modern world. This basic necessity is an <span>$875 billion</span> dollar global market.</p>
+            </div>
+            <div class="col-md-6">
+                <div class="global-market">
+
+                    <img src="../assets/img/global_market.png" alt="global">
+                </div>
+            </div>
+        </div>
+
+
     </div>
 </section>
 
